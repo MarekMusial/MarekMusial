0581b21473ed51f4cbb377349eb3f86d4edaa8be
MarekMusial <97898546+MarekMusial@users.noreply.github.com>
2023-02-23 13:04:46 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -4,21 +4,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Połączenie z bazą</title>
+    <title>Dodawanie ocen</title>
 </head>
 <body>
+    <header><h1>Dodawanie ocen</h1>
+</header>
+    <main>
     <?php
-    $polaczenie=mysqli_connect('localhost', 'root', '', '4ti');
-    if($polaczenie){
-        echo "połączyliśmy się z bazą";
-        $zapytanie="SELECT * FROM dane";
-        $wynik=mysqli_query($polaczenie,$zapytanie);
-        while($wiersz=mysqli_fetch_array($wynik)){
-            echo $wiersz['imie']." ".$wiersz['nazwisko']." - wiek: ".$wiersz['wiek']."<br>";
+        $connect=mysqli_connect('localhost', 'root', '', '4ti');
+        if($connect){
+            echo "połączyliśmy się z bazą<br>";
+            $zapytanie="SELECT * FROM przedmiot";
+            $wynik=mysqli_query($connect,$zapytanie);
+            while($wiersz=mysqli_fetch_array($wynik)){
+                $przedmiot[]=$wiersz['id'];
+                $idprzedmiot[]=$wiersz['przedmiot'];
+                $idprzedmiot2[]=$wiersz['nauczyciel']."<br>";
+            }
+        }else{
+            echo "błąd połączenia z bazą";
         }
-    }else{
-        echo "błąd połączenia z bazą"; 
-    }
+        mysqli_close($connect);
     ?>
+    <label for="przedmiot">Wybierz przedmiot</label>
+    <select name="przedmiot" id="przedmiot">
+    </select>
+    </main>
+    <footer>
+        <p>Stronę wykonał Marek</p>
+    </footer>
+    
 </body>
 </html>
