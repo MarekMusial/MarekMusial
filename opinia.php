673c7064e93ffc112d175ed23a8eee45d92a239b
MarekMusial <97898546+MarekMusial@users.noreply.github.com>
2023-03-29 10:25:19 +0200

    Add files via upload

---
+++ opinia.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Opinie klientów</title>
+    <link rel="stylesheet" href="styl3.css">
+</head>
+<body>
+    <header>
+    <h1>Hurtownia spożywcza</h1>
+    </header>
+    <main>
+        <h2>Opinie naszych klientów</h2>
+    <?php
+        $connect=mysqli_connect('localhost', 'root', '', 'hurtownia');
+        if($connect){
+        $zapytanie="SELECT zdjecie, imie, opinia FROM klienci INNER JOIN opinie ON klienci.id=opinie.klienci_id WHERE Klienci.Typy_id=2 OR Klienci.Typy_id=3;";
+        $wynik=mysqli_query($connect,$zapytanie);
+        while($wiersz=mysqli_fetch_array($wynik)){
+            echo "<img src='".$wiersz['zdjecie']."' alt='l;ient'>";
+            echo "<blockquote>".$wiersz['opinia']."</blockquote>";
+            echo "<h4>".$wiersz['imie']."</h4><br>"; 
+
+        }
+        
+    }   else{
+        echo "Błąd połączenia z bazą";
+    }
+    mysqli_close($connect);
+        
+    ?>
+    </main>
+    <footer class="stopka1">
+        <h3>Współpracują z nami</h3>
+        <a href="http://sklep.pl/">Sklep 1</a>
+    </footer>
+    <footer class="stopka2">
+        <h3>Nasi top klienci</h3>
+        <ol>
+            <li>#</li>
+            <li>#</li>
+            <li>#</li>
+            <li>#</li>
+        </ol>
+    </footer>
+    <footer class="stopka3">
+        <h3>Skontaktuj się</h3>
+        <p>telefon: 111222333</p>
+    </footer>
+    <footer class="stopka4">
+        <h3>Autor: 00000</h3>
+    </footer>
+</body>
+</html>
